4e71f8fc633f871f5ae211dbc6ab9a4c19c41078
Kjell-Magne <kjell_m.larsen@yahoo.com>
2023-11-20 21:40:25 +0100

    Ferdigstilt design og funksjonalitet
    Ferdig med design for index, product og about.html. Lagt til funksjonalitet for Google maps og Back to top knapp. Startet på Metadata.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// ########## Back to top button ##########
+//Kode fra W3Schools sin tutorial for å lage en Back to top knapp med Html, Css og Javascript: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// Get the button
+let mybutton = document.getElementById("toTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
