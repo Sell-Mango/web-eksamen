b7ba3ee83c2a2ec4af66c5077d1d735e533637e2
Kjell-Magne <kjell_m.larsen@yahoo.com>
2023-11-22 00:34:27 +0100

    Prosjekt ferdig

--- js/script.js
+++ js/script.js
@@ -13,9 +13,3 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
